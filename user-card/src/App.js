8239b8ce0429608c01608b84a651dbdf1c10f914
Sam Tarullo <sam@Sams-Air.attlocal.net>
2020-09-29 20:16:35 -0500

    finished with main tasks

---
+++ user-card/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import MyData from './MyData';
+import styled from 'styled-components';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      myData: [],
+      myFollowersUrls: [],
+      myFollowers: [],
+      followersToggled: false,
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/starullo')
+    .then(res=>{
+      this.setState({
+        myData: res.data,
+      });
+    });
+    axios.get('https://api.github.com/users/starullo/followers')
+    .then(data=>{
+      this.setState({
+        myFollowers: data.data
+      });
+    });
+
+
+
+  };
+
+  toggleFollowers = e => {
+    if (this.state.followersToggled) {
+      this.setState({
+        followersToggled: false,
+      })
+    } else {
+      this.setState({
+        followersToggled: true,
+      })
+    }
+  }
+
+  render() {
+    return(
+      <MyData toggleFollowers={this.toggleFollowers} state={this.state}/>
+    )
+  }
+}
+
+export default App;
